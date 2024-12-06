edbd14d89227b0ad02c2d92dc68da247b363f95f
Akanksha-Singla <92256793+Akanksha-Singla@users.noreply.github.com>
2024-12-06 10:28:42 +0530

    registeration and login done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { AllProductsComponent } from './views/all-products/all-products.component';
+export const routes: Routes = [
+{path:'',redirectTo:'login',pathMatch:"full"},
 
-export const routes: Routes = [];
+{
+    path:'login',
+    component:LoginComponent
+},
+
+{
+    path:'register',
+    component:RegisterComponent
+
+},
+{
+    path:'all-products',
+    component:AllProductsComponent
+
+}
+];
